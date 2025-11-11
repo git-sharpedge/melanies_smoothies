7b1861d9daa45bb188aac8b9ef653e63552555cc
git-sharpedge <git@sharpedge.se>
2025-11-11 13:43:01 +0100

    added extern connection

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 # Import python packages
 import streamlit as st
-#from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -10,9 +9,9 @@ st.write(
   """
 )
 
-session = get_active_session()
+con = st.connection("snowflake")
+session = con.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
 
 name_on_order = st.text_input('Name on Smoothie')
 st.write('The name mon your Smoothie will be: ', name_on_order)
@@ -23,13 +22,8 @@ if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-    st.write(ingredients_string)
-
-    
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order) values ('""" + ingredients_string + """','""" + name_on_order + """')"""
 
-    #st.write(my_insert_stmt)
-    #st.stop()
     time_to_insert = st.button('Submit order')
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
