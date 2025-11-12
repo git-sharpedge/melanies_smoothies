f6b38fd943cada9ae201b4b0d54fcebc2a8624fa
git-sharpedge <git@sharpedge.se>
2025-11-12 08:13:43 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
-import requets
+import requests
 
 
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
