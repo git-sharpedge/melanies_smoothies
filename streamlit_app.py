da6632b698daaf1a4fa41aa5b79f294695f73ef4
git-sharpedge <git@sharpedge.se>
2025-11-12 08:20:36 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,10 +4,6 @@ from snowflake.snowpark.functions import col
 import requests
 
 
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-#st.text(smoothiefroot_response.json())
-
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 # Write directly to the app
 st.title(f":cup_with_straw: Customize Your Smoothie")
 st.write(
@@ -28,6 +24,10 @@ if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
+  
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order) values ('""" + ingredients_string + """','""" + name_on_order + """')"""
 
     time_to_insert = st.button('Submit order')
