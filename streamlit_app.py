ab24c8fb118931a75726263c65b14cd13b826083
git-sharpedge <git@sharpedge.se>
2025-11-12 08:54:53 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,6 +22,7 @@ pd_df = my_dataframe.to_pandas()
 #st.dataframe(pd_df)
 #st.stop()
 
+
 name_on_order = st.text_input('Name on Smoothie')
 st.write('The name mon your Smoothie will be: ', name_on_order)
 
