54e757b2b6feea0010929b056f316c16c2a91193
git-sharpedge <git@sharpedge.se>
2025-11-12 08:51:34 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,9 +15,12 @@ st.write(
 con = st.connection("snowflake")
 session = con.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
-st.dataframe(data=my_dataframe, use_container_width=True)
-st.stop()
+#st.dataframe(data=my_dataframe, use_container_width=True)
+#st.stop()
 
+pd_df = my_dataframe.to_pandas()
+st.dataframe(pd_df)
+st.stop()
 
 name_on_order = st.text_input('Name on Smoothie')
 st.write('The name mon your Smoothie will be: ', name_on_order)
